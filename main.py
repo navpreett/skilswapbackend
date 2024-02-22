9a38375092fed5a08323bce522e4b28b724c37d6
jonasstDev <jonas.strehler@t-online.de>
2024-02-22 10:56:38 +0700

    added cors

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 CORS(app)
 
 
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+
+@app.after_request
+def after_request(response): return add_cors_headers(response)
+
 @app.route("/")
 def hello_world():
     response = jsonify({'message': 'Hello World!'})
