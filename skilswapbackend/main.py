e1b966f472a1805783b039770afaaa619de0b760
jonasstDev <jonas.strehler@t-online.de>
2024-02-29 11:36:24 +0700

    added reformatting with black

--- skilswapbackend/main.py
+++ skilswapbackend/main.py
@@ -7,25 +7,26 @@ CORS(app)
 
 
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
 
 @app.after_request
-def after_request(response): return add_cors_headers(response)
+def after_request(response):
+    return add_cors_headers(response)
+
 
 @app.route("/")
 def hello_world():
-    response = jsonify({'message': 'Hello World!'})
+    response = jsonify({"message": "Hello World!"})
     print("root route")
     return response
 
 
-app.register_blueprint(account_route, url_prefix='/account')
+app.register_blueprint(account_route, url_prefix="/account")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
--- skilswapbackend/routes/account/account.py
+++ skilswapbackend/routes/account/account.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, jsonify
 
-account_route = Blueprint('account_route', __name__)
+account_route = Blueprint("account_route", __name__)
 
 
-@account_route.route("/", methods=['Get'])
+@account_route.route("/", methods=["Get"])
 def account_index():
-
-    return jsonify({'message': 'Hello account management route!'})
+    return jsonify({"message": "Hello account management route!"})
